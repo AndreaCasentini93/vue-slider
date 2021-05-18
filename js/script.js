5ecd2b47a2304450fae2460d9fdac0c519d3f20f
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-18 16:22:02 +0200

    Setinterval

--- js/script.js
+++ js/script.js
@@ -1,48 +1,43 @@
-/*
-Rifare l'esercizio dello slider come fatto assieme in classe.
-
-Bonus:
-Gestire il cambio dell'immagine al click sui pallini in basso (nav);
-Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
-*/
-
 const app = new Vue (
     {
         el: "#root",
         data: {
-            img: {
-                src: [
-                    "img/img1.jpg",
-                    "img/img2.jpg",
-                    "img/img3.jpg",
-                    "img/img4.jpg",
-                    "img/img5.jpg",
-                    "img/img6.jpg",
-                    "img/img7.jpg"
-                ],
-                alt: "Immagine Paesaggio",
-                imageIndex: 0
-            }
+            images: [
+                "img/img1.jpg",
+                "img/img2.jpg",
+                "img/img3.jpg",
+                "img/img4.jpg",
+                "img/img5.jpg",
+                "img/img6.jpg",
+                "img/img7.jpg"
+            ],
+            altPlaceholder: "Immagine Paesaggio",
+            imageIndex: 0,
         },
         methods: {
             before: function() {
-                this.img.imageIndex--;
-                console.log(this.img.imageIndex);
+                this.imageIndex--;
 
-                if (this.img.imageIndex == -1) {
-                    this.img.imageIndex = (this.img.src.length - 1);
+                if (this.imageIndex == -1) {
+                    this.imageIndex = (this.images.length - 1);
                 }
-                console.log(this.img.imageIndex);
             },
             after: function() {
-                this.img.imageIndex++;
-                console.log(this.img.imageIndex);
+                this.imageIndex++;
 
-                if (this.img.imageIndex == this.img.src.length) {
-                    this.img.imageIndex = 0;
+                if (this.imageIndex == this.images.length) {
+                    this.imageIndex = 0;
                 }
-                console.log(this.img.imageIndex);
             }
+        },
+        created: function() {
+            const self = this;
+            setInterval(function() {
+                self.imageIndex++
+                if (self.imageIndex == self.images.length) {
+                    self.imageIndex = 0;
+                }                
+            }, 3000);
         }
     }
 );
