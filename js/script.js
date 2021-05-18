0aad4a7f754d1c5e206d65b0f5850cc6af642cb1
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-18 14:09:50 +0200

    Array Images

--- js/script.js
+++ js/script.js
@@ -10,7 +10,19 @@ const app = new Vue (
     {
         el: "#root",
         data: {
-            
+            img: {
+                src: [
+                    "img/img1.jpg",
+                    "img/img2.jpg",
+                    "img/img3.jpg",
+                    "img/img4.jpg",
+                    "img/img5.jpg",
+                    "img/img6.jpg",
+                    "img/img7.jpg"
+                ],
+                alt: "Immagine Paesaggio",
+                imageIndex: 0
+            }
         },
         methods: {
 
