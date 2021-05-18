7b08db68f55bc98c4c75794731782cfbf212a435
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-18 13:22:30 +0200

    Files and Folders

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+Rifare l'esercizio dello slider come fatto assieme in classe.
+
+Bonus:
+Gestire il cambio dell'immagine al click sui pallini in basso (nav);
+Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+*/
