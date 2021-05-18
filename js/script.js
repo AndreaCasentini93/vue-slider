beb3033ec4864c1a558e096f134e8a9890577bdf
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-18 13:36:28 +0200

    Images

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ Bonus:
 Gestire il cambio dell'immagine al click sui pallini in basso (nav);
 Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
 */
+
+const app = new Vue (
+    {
+        el: "#root",
+        data: {
+            
+        },
+        methods: {
+
+        }
+    }
+);
